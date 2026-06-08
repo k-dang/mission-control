1cd9d7512dc414e3b82b20ad1e0e00838ee9df4a
Kevin Dang <dangkevin12@gmail.com>
2026-06-08 01:12:02 -0400

    Use next script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { shadcn } from "@clerk/ui/themes";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ConvexClientProvider from "@/components/ConvexClientProvider";
 import { AppShell } from "@/components/app-shell";
@@ -28,18 +29,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <head>
-        {process.env.NODE_ENV === "development" ? (
-          // eslint-disable-next-line @next/next/no-sync-scripts
-          <script
-            crossOrigin="anonymous"
-            src="https://cdn.jsdelivr.net/npm/react-scan/dist/auto.global.js"
-          />
-        ) : null}
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {process.env.NODE_ENV === "development" ? (
+          <Script
+            src="https://cdn.jsdelivr.net/npm/react-scan/dist/auto.global.js"
+            strategy="beforeInteractive"
+            crossOrigin="anonymous"
+          />
+        ) : null}
         <ClerkProvider appearance={{ theme: shadcn }}>
           <ConvexClientProvider>
             <AppShell>{children}</AppShell>
